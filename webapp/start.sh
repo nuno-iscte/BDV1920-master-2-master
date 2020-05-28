71db8eb9116f046e2ac470ce8eb01a569abed07a
carlos-iscte <64497499+carlos-iscte@users.noreply.github.com>
2020-05-28 20:31:56 +0100

    Revert "Remocao dos CR"
    This reverts commit 77877588139593b32825596d1e4b7ec5c3051761.

--- webapp/start.sh
+++ webapp/start.sh
@@ -2,8 +2,7 @@
 
 gunicorn --bind=0.0.0.0:5000 --keep-alive=2000 \
     --timeout=2000 --log-level=debug  --reload \
-	flaskacidentesapp:app
-    #flaskcovid19app:app
+    flaskcovid19app:app
     #flaskmoviesapp:app
 
 # apps presented as examples throughout the course
