5a9f4b36337b1b53de537931a3978fc14e5ebd0a
miguel4391 <miguel4391@gmail.com>
2020-06-03 23:50:28 +0100

    updae graph diretion

--- webapp/static/js/weather.js
+++ webapp/static/js/weather.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = {top: 40, right: 40, bottom: 30, left: 110},
+var margin = {top: 10, right: 40, bottom: 70, left: 110},
     width = 400 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
@@ -19,7 +19,7 @@ var parseTime = d3.timeParse('%m/%_d/%y')
 // Create data
 function drawData(data){
     data.sort(function (a, b){
-        return a.N_Acidentes - b.N_Acidentes;
+        return b.N_Acidentes - a.N_Acidentes;
     })
   updateData(data);
 }
@@ -27,10 +27,11 @@ function drawData(data){
 function updateData(data){
   console.log(data);
 
-  // Add X axis --> it is a date format
-  var x = d3.scaleLinear()
-    .domain(d3.extent(data, function(d) { return d.N_Acidentes; }))
-    .range([ 0, width ]);
+  // Add Y axis 
+  var x = d3.scaleBand()
+    .domain(data.map(function(d) { return d.DESC_DISTRITO; }))
+    .range([ 0, width ])
+    .padding(0.2);
   xAxis = svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x))
@@ -39,10 +40,9 @@ function updateData(data){
     .style("text-anchor", "end");
 
   // Add Y axis
-  var y = d3.scaleBand()
-    .domain(data.map(function(d) { return d.DESC_DISTRITO; }))
+  var y = d3.scaleLinear()
+    .domain(d3.extent(data, function(d) { return d.N_Acidentes; }))
     .range([ height, 0 ])
-    .padding(.1);
   yAxis = svg.append("g")
     .call(d3.axisLeft(y));
   
@@ -61,9 +61,9 @@ function updateData(data){
   .data(data)
   .enter()
   .append("rect")
-  .attr("x", x(0) )
-  .attr("y", function(d) { return y(d.DESC_DISTRITO); })
-  .attr("width", function(d) { return x(d.N_Acidentes); })
-  .attr("height", y.bandwidth() )
-  .attr("fill", "#69b3a2")
+  .attr("x", function(d) { return x(d.DESC_DISTRITO); })
+      .attr("y", function(d) { return y(d.N_Acidentes); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.N_Acidentes); })
+      .attr("fill", "#69b3a2")
 }
