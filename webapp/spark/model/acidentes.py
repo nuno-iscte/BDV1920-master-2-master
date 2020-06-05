00072fb268826a5e0a53370c19f6e35e90bdf46a
carlos-iscte <64497499+carlos-iscte@users.noreply.github.com>
2020-06-05 21:22:10 +0100

    Qt_feridos, qt_mortos, qt_feridos

--- webapp/spark/model/acidentes.py
+++ webapp/spark/model/acidentes.py
@@ -142,7 +142,8 @@ class AcidentesModel:
 	
 	def get_crossfilter(self, spark):
 		df = spark.sql("SELECT CONCAT(SUBSTR(DATA,3,2), SUBSTRING_INDEX(DATA, '/', 1), SUBSTR(Hora,1,2),SUBSTR(Hora,4,2) ) as DataAcidente, \
-			Tipo_Natureza as T_natureza, FACTOR_ATMOSFERICO as F_Atmosferico, GRAVIDADE FROM acidentes")	
+			QT_MORTOS, QT_FGRAVES, QT_FLIGEIROS, \
+				 Tipo_Natureza as T_natureza, FACTOR_ATMOSFERICO as F_Atmosferico, GRAVIDADE FROM acidentes")	
 		listing = df.toPandas().to_dict(orient='records') 
 		jsonlisting = json.dumps(listing, indent=2)
 		return jsonlisting
